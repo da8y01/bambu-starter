6c714d6583235439bebbf5df8d7d0eccb5d4baf4
Developer Localhost <da8y_01@yahoo.com>
2023-09-29 14:51:14 -0500

    Added after git clone.

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  ChainId,
+  rainbowWallet,
+} from "@thirdweb-dev/react";
+import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ThirdwebProvider desiredChainId={ChainId.Mumbai}
+  activeChain={'mumbai'}
+  supportedWallets={[
+    metamaskWallet(),
+    coinbaseWallet(),
+    walletConnect(),
+    rainbowWallet(),
+  ]}> 
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider> 
+)
