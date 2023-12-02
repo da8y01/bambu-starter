a443070dbf02f67ccd48fd1fee11eb39f6758b9f
Developer Localhost <da8y_01@yahoo.com>
2023-12-02 13:57:01 -0500

    Copy dist/ to docs/ for classic GH Pages deploy..

---
+++ docs/assets/hasERC20Allowance-af8c96d2.browser.esm.ad3ab689.js
@@ -0,0 +1,1 @@
+import{C as c,ag as d}from"./index.58457bc4.js";async function w(a,e,n){const r=a.getProvider(),s=new c(r,e,d,{},a.storage),t=await a.getSignerAddress(),o=a.readContract.address;return(await s.readContract.allowance(t,o)).gte(n)}export{w as h};
