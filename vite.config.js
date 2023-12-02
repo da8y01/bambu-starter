b4341713a14d980161303c8714642ca1125f9e91
Developer Localhost <da8y_01@yahoo.com>
2023-12-02 13:20:40 -0500

    Add base to vite config.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bambu-starter/',
   build: {
     outDir: "dist",
   },
